9621ecd6fcd9c0d2a03b40f36868dbaf43e360f0
Majikat <dan.raoelinarivo@live.fr>
2024-03-21 10:34:17 +0300

    Use 4 core systems on ECS.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,7 @@ module.exports = {
     env: {
       PORT: 14047
     }
-  }, 
-  {
+  }, {
     name: 'Procell Website 14048',
     script: 'npm',
     args: 'start',
@@ -23,6 +22,30 @@ module.exports = {
     exec_mode: 'cluster',
     env: {
       PORT: 14048
+    }
+  }, {
+    name: 'Procell Website 14049',
+    script: 'npm',
+    args: 'start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '2G',
+    exec_mode: 'cluster',
+    env: {
+      PORT: 14049
+    }
+  }, {
+    name: 'Procell Website 14050',
+    script: 'npm',
+    args: 'start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '2G',
+    exec_mode: 'cluster',
+    env: {
+      PORT: 14050
     }
   }]
 };
