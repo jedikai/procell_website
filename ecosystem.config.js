7e69b02148ff98d72e0eeb5cfee77d1d7c5828cf
Majikat <dan.raoelinarivo@live.fr>
2024-03-25 16:50:34 +0300

    Reverting to 2 cores per instance.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,29 +23,5 @@ module.exports = {
     env: {
       PORT: 14048
     }
-  }, {
-    name: 'Procell Website 14049',
-    script: 'npm',
-    args: 'start',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '2G',
-    exec_mode: 'cluster',
-    env: {
-      PORT: 14049
-    }
-  }, {
-    name: 'Procell Website 14050',
-    script: 'npm',
-    args: 'start',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '2G',
-    exec_mode: 'cluster',
-    env: {
-      PORT: 14050
-    }
   }]
 };
